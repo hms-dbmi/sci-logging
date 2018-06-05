58a2e72c03cd400904f607029fd3b5da565bc13c
mtmcduffie <mcduffie.michael@gmail.com>
2018-06-05 11:45:28 -0400

    Tick version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='sci-logging',
-    version='0.1',
+    version='0.2',
     packages=['scilogging',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     install_requires=[""],
